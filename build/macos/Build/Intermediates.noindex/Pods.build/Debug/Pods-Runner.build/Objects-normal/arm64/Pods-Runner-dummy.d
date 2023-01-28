08c5d3cf088ad54ead2f62710678d7c3e3798783
ishakdas <furkan.ishack@gmail.com>
2023-01-28 16:25:22 +0300

    flutter clean

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/yahyaizala/Desktop/Apps/whatshafiz/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/yahyaizala/Desktop/Apps/whatshafiz/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/Objects-normal/arm64/shared_preferences_macos_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/yahyaizala/Desktop/Apps/whatshafiz/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_macos.build/DerivedSources/shared_preferences_macos_vers.c
